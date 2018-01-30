2b04bca60887067b51cf72ad440c4b05493e2aa1
Tomohito Yamazaki <t07840ty@gmail.com>
2018-01-30 10:23:36 +0900

    "Add git tool for all students' repositories"

---
+++ remoteaddall.sh
@@ -0,0 +1,12 @@
+echo "please input your repo's name: "
+read REPO_NAME
+git remote add 01 git@github.com:arschool01/${REPO_NAME}.git
+git remote add 02 git@github.com:arschool02/${REPO_NAME}.git
+git remote add 03 git@github.com:arschool03/${REPO_NAME}.git
+git remote add 04 git@github.com:arschool04/${REPO_NAME}.git
+git remote add 05 git@github.com:arschool05/${REPO_NAME}.git
+git remote add 06 git@github.com:arschool06/${REPO_NAME}.git
+git remote add 07 git@github.com:arschool07/${REPO_NAME}.git
+git remote add 08 git@github.com:arschool08/${REPO_NAME}.git
+git remote add 09 git@github.com:arschool09/${REPO_NAME}.git
+git remote add 10 git@github.com:arschool10/${REPO_NAME}.git
